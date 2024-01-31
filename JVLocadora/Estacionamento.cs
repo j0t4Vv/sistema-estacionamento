68f4ed85a6e33979bc80a787cce9b8f7ec6e9af9
João V. S. Oliveira <145166602+j0t4Vv@users.noreply.github.com>
2024-01-31 19:03:23 -0300

    Add files via upload

---
+++ JVLocadora/Estacionamento.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JVLocadora
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        //ctor
+        public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo");
+            string placa = Console.ReadLine().ToUpper();
+
+            if(veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Já existe um veículo com essa placa");
+            }
+            else
+            {
+                veiculos.Add(placa);
+                Console.WriteLine("Veículo cadastrado com sucesso");
+            }
+
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa de veículo");
+            string placa = Console.ReadLine().ToUpper();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo ficou no estacionamento");
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal precoTotal = precoInicial + precoHora * horas;
+                veiculos.Remove(placa);
+                Console.WriteLine($"O veículo de placa {placa} e deve pagar R${precoTotal:F2}");
+
+            }
+            else
+            {
+                Console.WriteLine("O veículo não foi encontrado em nosso pátio");
+            }
+        }
+
+        public void ListarVeiculo()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículo estacionados são");
+                foreach (var v in veiculos)
+                {
+                    Console.WriteLine(v);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos em nosso pátio");
+            }
+        }
+    }
+}
---
+++ JVLocadora/Program.cs
@@ -0,0 +1,59 @@
+﻿//Declarando variáveis
+
+using JVLocadora;
+
+decimal precoInicial = 0;
+decimal precoHora  = 0;
+
+//Iniciar o sistema
+
+Console.WriteLine("Seja bem vindo ao estacionamento do JV");
+Console.WriteLine("---------------------------------------");
+
+Console.WriteLine("Digite o preço inicial");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora");
+precoHora = Convert.ToDecimal(Console.ReadLine());
+
+//Instancia à classe estacionamento
+
+Estacionamento es = new Estacionamento(precoInicial, precoHora);
+
+//Loop
+
+bool menu = true;
+
+while (menu)
+{
+    Console.Clear();
+    Console.WriteLine("Escolha uma opção");
+    Console.WriteLine("1 - Adicionar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículo");
+    Console.WriteLine("4 - Encerrar sistema");
+
+    switch(Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+        case "2":
+            es.RemoverVeiculo();
+            break;
+        case "3":
+            es.ListarVeiculo();
+            break;
+        case "4":
+            menu = false;
+            break;
+        default:
+            Console.WriteLine("Digite uma opção válida");
+            break;
+    }
+    Console.WriteLine("Digite uma tecla para confirmar");
+    Console.ReadLine();
+
+}
+
+Console.WriteLine("O programa encerrou! ");
